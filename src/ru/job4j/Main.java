4f2a824179f0d89959a631d38f5378523b0bcd37
dkuznetsov1989 <a693kn@yandex.ru>
2020-05-25 12:43:20 +0300

    9.2. Checkstyle [#295355]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 public class Main {
+    @SuppressWarnings("checkstyle:NoWhitespaceAfter")
     public static void main(String [] args) {
-
         System.out.println("Hello world");
     }
 }
