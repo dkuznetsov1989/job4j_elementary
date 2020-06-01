6fd96902742b5d46287176e3f104b6dc6197e215
dkuznetsov1989 <a693kn@yandex.ru>
2020-06-01 11:16:12 +0300

    5.8. JavaDoc. Code convention [#295402]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,32 @@
 package ru.job4j.converter;
 
+/**
+ * Конвертация
+ *
+ * "author Dmitry Kuznetsov (a693kn@yandex.ru)
+ */
+
 public class Converter {
     public static int rubleToEuro(int value) {
+        /**
+         * Convertation ruble to Euro
+         */
         int rsl = value / 70;
         return rsl;
     }
 
     public static int rubbleToDollar(int value) {
+        /**
+         * Convertation ruble to Dollars
+         */
         int rsl = value / 60;
         return rsl;
     }
 
+    /**
+     * Main
+     * @param args - args.
+     */
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
